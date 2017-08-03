2935ef8bba93575c62f4bdf44a7652da397fd992
Victor <dfdhby.r@gmail.com>
2017-08-03 23:01:58 +0300

    done

--- levyt/assets/js/scripts.js
+++ levyt/assets/js/scripts.js
@@ -1,15 +1,38 @@
 $(function() {
 
+
+	$(document).ready(function(){
+		$(".icon-menu").click(function(){
+			$("nav ul").slideToggle();
+		});
+	});
+
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("form").submit(function() { //Change
+	$("form#form-number2").submit(function() { //Change
 		var th = $(this);
 		$.ajax({
 			type: "POST",
-			url: "mail.php", //Change
+			url: "/mail.php", //Change
 			data: th.serialize()
 		}).done(function() {
-			alert("Thank you!");
+			$('#exampleModal').arcticmodal();
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	$("form#form-number").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "/cart.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$('#exampleModal').arcticmodal();
 			setTimeout(function() {
 				// Done Functions
 				th.trigger("reset");
